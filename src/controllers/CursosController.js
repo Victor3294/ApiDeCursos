672123508c8fb98ccdb0a4677030ee4d337a008f
Victor3294 <vblum26@gmail.com>
2024-07-14 15:22:29 -0300

    feat: 🎸 Resolução do exercicio 04

--- src/controllers/CursosController.js
+++ src/controllers/CursosController.js
@@ -56,6 +56,25 @@ class CursosController {
             console.log(error)
         }
     }
+
+    async atualizarCurso (request, response) {
+        try {
+            const dados = request.body
+            const id = request.params.id
+            const curso = await Curso.findByPk(id)
+            if(!curso){
+                return response.status(404).json({mensagem: "Não foi encontrado um curso com esse id"})
+            }
+            curso.nome = dados.nome
+            curso.duracao = dados.duracao
+
+            await curso.save()
+            response.status(200).json(curso)
+
+        } catch (error) {
+            response.status(500).json({mensagem: "Não foi possivel atualizar o curso"})
+        }
+    }
 }
 
 module.exports = new CursosController()
