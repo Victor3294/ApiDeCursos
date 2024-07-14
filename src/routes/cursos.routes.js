99b8aec3f5cb87af6ecb1bf8587516c7de2920dc
Victor3294 <vblum26@gmail.com>
2024-07-14 15:29:53 -0300

    feat: 🎸 Resolução do exercicio 05

--- src/routes/cursos.routes.js
+++ src/routes/cursos.routes.js
@@ -7,5 +7,6 @@ cursosRoutes.post('/', CursosController.criar)
 cursosRoutes.get('/', CursosController.listarTodos)
 cursosRoutes.get('/l', CursosController.buscarCursos)
 cursosRoutes.put('/:id', CursosController.atualizarCurso)
+cursosRoutes.delete('/:id', CursosController.deletarCurso)
 
 module.exports = cursosRoutes
